497c1ff670466d57fa3fb36f1e831d3d76d8047e
Subeesh Sudhakaran <subeesh.classroom@gmail.com>
2021-10-09 23:31:14 +0400

    Added Default Routes

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -7,13 +7,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        //primarySwatch: Colors.red, // Can overwrite the color
-      ),
-    );
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          //primarySwatch: Colors.red, // Can overwrite the color
+        ),
+        routes: {
+          "/": (context) => Home(),
+        });
   }
 }
