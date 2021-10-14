4959e7a9da88126417c86f311f174fd35a410e6c
Subeesh Sudhakaran <subeesh.classroom@gmail.com>
2021-10-15 00:16:45 +0400

    Learned Sized box, Padding,TextFormInout,Decoration, hintText, labeltext, obsecureText

--- my_app/lib/screens/login.dart
+++ my_app/lib/screens/login.dart
@@ -6,6 +6,9 @@ class Login extends StatelessWidget {
     return Material(
       child: Column(
         children: [
+          const SizedBox(
+            height: 20.0,
+          ),
           Image.asset(
             "assets/images/logIn.png",
             fit: BoxFit.cover,
@@ -16,8 +19,32 @@ class Login extends StatelessWidget {
           const Text(
             "Welcome",
             style: TextStyle(
-              fontSize: 22,
+              fontSize: 28,
               fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                )
+              ],
             ),
           ),
           const SizedBox(
