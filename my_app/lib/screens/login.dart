2defc617c988ac05aa93ad6fbee6faa4a9bb42c1
Subeesh Sudhakaran <subeesh.classroom@gmail.com>
2021-10-21 11:59:00 +0400

    Single Child Scroll View

--- my_app/lib/screens/login.dart
+++ my_app/lib/screens/login.dart
@@ -3,61 +3,63 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 100.0,
-          ),
-          Image.asset(
-            "assets/images/logIn.png",
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Text(
-            "Welcome New MPL",
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Material(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100.0,
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                )
-              ],
+            Image.asset(
+              "assets/images/logIn.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-            child: Text("Log In"),
-            style: TextButton.styleFrom(),
-            onPressed: () {
-              print("Welcome to MPL");
-            },
-          )
-        ],
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome New MPL",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 250.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              child: Text("Log In"),
+              style: TextButton.styleFrom(),
+              onPressed: () {
+                print("Welcome to MPL");
+              },
+            )
+          ],
+        ),
       ),
     );
   }
